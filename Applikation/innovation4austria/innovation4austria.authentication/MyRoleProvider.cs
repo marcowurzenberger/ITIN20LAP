839d30b7abec965349ccb3dd32fa1d143d0be429
wurzmarc <wurzmarc@PC001999F94098>
2017-01-24 09:06:52 +0100

    RoleProvider weiter bearbeitet

--- Applikation/innovation4austria/innovation4austria.authentication/MyRoleProvider.cs
+++ Applikation/innovation4austria/innovation4austria.authentication/MyRoleProvider.cs
@@ -67,13 +67,21 @@ namespace innovation4austria.authentication
                 {
                     using (var context = new innovations4austriaEntities())
                     {
-                        
+                        foreach (var u in context.portalusers)
+                        {
+                            if (u.email == username)
+                            {
+                                roles[0] = u.role.description;
+                            }
+                        }
+
+                        return roles;
                     }
                 }
                 else
                 {
                     log.Warn("username is null or empty!");
-                    return null;
+                    return roles = null;
                 }
             }
             catch (Exception ex)
@@ -87,12 +95,77 @@ namespace innovation4austria.authentication
 
         public override string[] GetUsersInRole(string roleName)
         {
-            throw new NotImplementedException();
+            XmlConfigurator.Configure();
+
+            log.Info("GetUsersInRole(string roleName)");
+
+            string[] users = new string[100];
+
+            try
+            {
+                if (!string.IsNullOrEmpty(roleName))
+                {
+                    using (var context = new innovations4austriaEntities())
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    log.Warn("roleName is null or empty");
+                    return users = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error getting all users in role");
+                Debugger.Break();
+            }
+
+            return users;
         }
 
         public override bool IsUserInRole(string username, string roleName)
         {
-            throw new NotImplementedException();
+            XmlConfigurator.Configure();
+
+            log.Info("IsUserInRole(string username, string roleName)");
+
+            bool exist = false;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(roleName))
+                {
+                    using (var context = new innovations4austriaEntities())
+                    {
+                        foreach (var r in context.roles)
+                        {
+                            if (r.description == roleName)
+                            {
+                                foreach (var u in r.portalusers)
+                                {
+                                    if (username == u.email)
+                                    {
+                                        return exist = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    return exist = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error at IsUserInRole", ex);
+                Debugger.Break();
+            }
+
+            return exist;
         }
 
         public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
